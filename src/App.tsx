7feebf675a7e0d54695441b0557b2d90cc3377bb
Cmmh1101 <56605042+Cmmh1101@users.noreply.github.com>
2022-03-28 22:23:47 -0500

    added font source, changed styles to scss files

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
-import "./App.css";
+import "./App.scss";
 import Layout from "./layout/Layout";
 import ScrollToTop from "./ScrollToTop";
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "@fontsource/poppins";
 import "bootstrap/dist/css/bootstrap.min.css";
 // import "bootstrap/dist/css/bootstrap.css";
-import "~bootstrap/scss/bootstrap.scss";
-import "./index.css";
+import "./index.scss";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
