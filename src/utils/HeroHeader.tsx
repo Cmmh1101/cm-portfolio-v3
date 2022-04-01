2d11ce2301125bfa14cdc5fcf02dfdaa5aca89c1
Cmmh1101 <56605042+Cmmh1101@users.noreply.github.com>
2022-03-31 23:12:25 -0500

    hero links

--- src/utils/HeroHeader.tsx
+++ src/utils/HeroHeader.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Button } from "reactstrap";
 import { useTheme } from "../provider/ThemeModeProvider";
 
 interface Props {
@@ -6,9 +7,18 @@ interface Props {
   enText: string;
   spTitle: string;
   spText: string;
+  enButton: string;
+  esButton: string;
 }
 
-const HeroHeader = ({ enText, enTitle, spTitle, spText }: Props) => {
+const HeroHeader = ({
+  enText,
+  enTitle,
+  spTitle,
+  spText,
+  enButton,
+  esButton,
+}: Props) => {
   const { englishMode } = useTheme();
   return (
     <div className="hero-text">
@@ -16,11 +26,17 @@ const HeroHeader = ({ enText, enTitle, spTitle, spText }: Props) => {
         <>
           <h1>{spTitle}</h1>
           <p>{spText}</p>
+          <Button outline type="button">
+            {esButton}
+          </Button>
         </>
       ) : (
         <>
           <h1>{enTitle}</h1>
           <p>{enText}</p>
+          <Button outline type="button">
+            {enButton}
+          </Button>
         </>
       )}
     </div>
--- src/views/homepage/components/Hero.tsx
+++ src/views/homepage/components/Hero.tsx
@@ -1,17 +1,41 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
-import { Container } from "reactstrap";
+import { Col, Container, Row } from "reactstrap";
 import HeroHeader from "../../../utils/HeroHeader";
+import {
+  faLinkedin,
+  faGithub,
+  faGithubSquare,
+} from "@fortawesome/free-brands-svg-icons";
+import { Link } from "react-router-dom";
 
 const Hero = () => {
   return (
     <Container tag="header" fluid className="hero-container">
-      <HeroHeader
-        spTitle="Ingeniera de Sistemas"
-        spText="Resolviendo problemas de los negocios con desarrollo de
+      <Row>
+        <Col
+          xs={2}
+          className="justify-content-center d-flex flex-column align-items-end follow-icons"
+        >
+          <Link to="/">
+            <FontAwesomeIcon icon={faLinkedin} className="icon mb-3" />
+          </Link>
+          <Link to="/">
+            <FontAwesomeIcon icon={faGithubSquare} className="icon mt-3" />
+          </Link>
+        </Col>
+        <Col xs={10}>
+          <HeroHeader
+            spTitle="Ingeniera de Sistemas"
+            spText="Resolviendo problemas de los negocios con desarrollo de
               aplicationes web"
-        enTitle="Software Engineer"
-        enText="Solving Problems with full stack web development Solutions"
-      />
+            enTitle="Software Engineer"
+            enText="Solving Problems with full stack web development Solutions"
+            enButton="Recent Projects"
+            esButton="Proyectos Recientes"
+          />
+        </Col>
+      </Row>
     </Container>
   );
 };
