ab617641ffb1bb59c675620749c7b347bd7a11a4
Cmmh1101 <56605042+Cmmh1101@users.noreply.github.com>
2022-04-13 08:31:21 -0500

    refactored projects list

--- src/views/homepage/components/ProjectsList.tsx
+++ src/views/homepage/components/ProjectsList.tsx
@@ -32,18 +32,12 @@ export interface Project {
 const ProjectsList = () => {
   const { englishMode, darkMode, dark } = useTheme();
 
-  const [frontVisible, setFrontVisible] = useState(false);
-  const [fullStackVisible, setFullStackVisible] = useState(false);
-
-  const [cmsVisible, setCmsVisible] = useState(false);
-
-  const [allProjects, setAllProjects] = useState(true);
-
   const front = projectsData
     .filter((p) => p.type === "frontend")
     .map((p) => {
       return (
         <Project
+          key={p.name}
           name={p.name}
           customClass={p.customClass}
           enAlt={p.enAlt}
@@ -64,6 +58,7 @@ const ProjectsList = () => {
     .map((p) => {
       return (
         <Project
+          key={p.name}
           name={p.name}
           customClass={p.customClass}
           enAlt={p.enAlt}
@@ -84,6 +79,7 @@ const ProjectsList = () => {
     .map((p) => {
       return (
         <Project
+          key={p.name}
           name={p.name}
           customClass={p.customClass}
           enAlt={p.enAlt}
@@ -102,6 +98,7 @@ const ProjectsList = () => {
   const all = projectsData.map((p) => {
     return (
       <Project
+        key={p.name}
         name={p.name}
         customClass={p.customClass}
         enAlt={p.enAlt}
@@ -118,61 +115,39 @@ const ProjectsList = () => {
     );
   });
 
+  const [filteredProjects, setFilteredPropjects] = useState<any>(all);
+
   const handleFrontButton = () => {
-    setCmsVisible(false);
-    setAllProjects(false);
-    setFullStackVisible(false);
-    setFrontVisible(true);
+    setFilteredPropjects(front);
     console.log(front);
   };
   const handleFullButton = () => {
-    setFrontVisible(false);
-    setCmsVisible(false);
-    setAllProjects(false);
-    setFullStackVisible(true);
+    setFilteredPropjects(fullstack);
+
     console.log(fullstack);
   };
   const handleCmsButton = () => {
-    setFrontVisible(false);
-    setFullStackVisible(false);
-    setAllProjects(false);
-    setCmsVisible(true);
+    setFilteredPropjects(cms);
   };
   const handleAllButton = () => {
-    setFrontVisible(true);
-    setFullStackVisible(true);
-    setCmsVisible(true);
+    setFilteredPropjects(all);
     console.log(all);
   };
 
   return (
     <div>
       <ButtonGroup className="col-12 button-group">
-        <Button active={allProjects} onClick={() => handleAllButton()}>
-          All
-        </Button>
-        <Button active={frontVisible} onClick={() => handleFrontButton()}>
-          Front End
-        </Button>
-        <Button active={fullStackVisible} onClick={() => handleFullButton()}>
-          Full Stack
-        </Button>
-        <Button active={cmsVisible} onClick={() => handleCmsButton()}>
-          CMS
-        </Button>
+        <Button onClick={() => handleAllButton()}>All</Button>
+        <Button onClick={() => handleFrontButton()}>Front End</Button>
+        <Button onClick={() => handleFullButton()}>Full Stack</Button>
+        <Button onClick={() => handleCmsButton()}>CMS</Button>
       </ButtonGroup>
       <Row>
         <Col
           xs={12}
-          className="d-flex flex-wrap justify-content-center align-items-center"
+          className="d-flex flex-wrap justify-content-evenly align-items-center"
         >
-          {frontVisible
-            ? front
-            : fullStackVisible
-            ? fullstack
-            : cmsVisible
-            ? cms
-            : allProjects && all}
+          {filteredProjects}
         </Col>
       </Row>
     </div>
