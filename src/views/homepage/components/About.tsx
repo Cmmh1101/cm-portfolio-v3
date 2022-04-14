9e29c34906b7dcec2de9d75cd99972366a7f146d
Cmmh1101 <56605042+Cmmh1101@users.noreply.github.com>
2022-04-14 09:31:28 -0500

    about placeholer en and sp text

--- src/views/homepage/components/About.tsx
+++ src/views/homepage/components/About.tsx
@@ -6,25 +6,18 @@ import SubHeader from "../../../utils/SubHeader";
 const About = () => {
   const { englishMode } = useTheme();
 
-  const description = [
-    {
-      aboutEn: [
-        `I'm a passionate self-taught Web Developer and Designer. My proactive approach has resulted in numerous important wins in my professional development in tech, teaching, and sports.`,
-        `Being able to plan, implement, supervise, evaluate, communicate, receive feedback, and correct are skills that combined with my Tech knowledge and Hispanic background give me a great base to help you grow and reach your goals.`,
-        `Since my Web Developer journey started I found out that with tech I can do the things that I enjoy the most, be creative, solve problems, overcome challenges, create solutions, and the most important to help others achieve their goals.`,
-      ],
-    },
-    {
-      aboutEsp: [
-        `Soy una Ingeniera de Software apasionada y preparada independientemente. My proactive approach has resulted in numerous important wins in my professional development in tech, teaching, and sports.`,
-        `Being able to plan, implement, supervise, evaluate, communicate, receive feedback, and correct are skills that combined with my Tech knowledge and Hispanic background give me a great base to help you grow and reach your goals.`,
-        `Since my Web Developer journey started I found out that with tech I can do the things that I enjoy the most, be creative, solve problems, overcome challenges, create solutions, and the most important to help others achieve their goals.`,
-      ],
-    },
+  const aboutEn = [
+    "I'm a passionate self-taught Web Developer and Designer. My proactive approach has resulted in numerous important wins in my professional development in tech, teaching, and sports.",
+    "Being able to plan, implement, supervise, evaluate, communicate, receive feedback, and correct are skills that combined with my Tech knowledge and Hispanic background give me a great base to help you grow and reach your goals.",
+    "Since my Web Developer journey started I found out that with tech I can do the things that I enjoy the most, be creative, solve problems, overcome challenges, create solutions, and the most important to help others achieve their goals.",
   ];
 
-  const spanish = description.map((a, i) => <p key={i}>{a.aboutEsp}</p>);
-  const english = description.map((a, i) => <p key={i}>{a.aboutEn}</p>);
+  const aboutEsp = [
+    `Soy una Ingeniera de Software apasionada y preparada independientemente. My proactive approach has resulted in numerous important wins in my professional development in tech, teaching, and sports.`,
+    `Being able to plan, implement, supervise, evaluate, communicate, receive feedback, and correct are skills that combined with my Tech knowledge and Hispanic background give me a great base to help you grow and reach your goals.`,
+    `Since my Web Developer journey started I found out that with tech I can do the things that I enjoy the most, be creative, solve problems, overcome challenges, create solutions, and the most important to help others achieve their goals.`,
+  ];
+
   return (
     <>
       <Container>
@@ -32,7 +25,15 @@ const About = () => {
           <Col xs={12} className="my-5 projects-section">
             <SubHeader enTitle="About Me" spTitle="Sobre Mi" />
           </Col>
-          <Col xs={12}>{englishMode ? { spanish } : { english }}</Col>
+          <Col xs={12}>
+            {englishMode
+              ? aboutEsp.map((parragraph, i) => {
+                  return <p key={i}>{parragraph}</p>;
+                })
+              : aboutEn.map((parragraph, i) => {
+                  return <p key={i}>{parragraph}</p>;
+                })}
+          </Col>
         </Row>
       </Container>
     </>
