0716652dc42e0d3b99e0158fdc6cf30e28baa8e2
Cmmh1101 <56605042+Cmmh1101@users.noreply.github.com>
2022-04-16 23:29:54 -0500

    added skills images

---
+++ src/skillsData.ts
@@ -0,0 +1,22 @@
+const projects = [
+      {
+      name: "adobe",
+      title: "Adobe XD",
+      pageLink: "../images/skills/cib-adobe-xd.svg",
+      type: "soft"
+      },
+      {
+      name: "aws",
+      title: "AWS",
+      pageLink: "../images/skills/cib-amazon-aws.svg",
+      type: "soft"
+      },
+      {
+      name: "adobe",
+      title: "Adobe XD",
+      pageLink: "https://blog.carlamontano.tech",
+      type: "soft"
+      },
+];
+
+export default projects;
