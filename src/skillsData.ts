067acec20d86c58adf4e16783a93bfe6e18f24b0
Cmmh1101 <56605042+Cmmh1101@users.noreply.github.com>
2022-04-17 00:45:52 -0500

    adding images routes

--- src/skillsData.ts
+++ src/skillsData.ts
@@ -1,21 +1,125 @@
 const projects = [
       {
-      name: "adobe",
-      title: "Adobe XD",
-      pageLink: "../images/skills/cib-adobe-xd.svg",
-      type: "soft"
+      name: "git",
+      title: "Git",
+      pageLink: "../images/skills/cib-git.svg",
+      type: "git"
+      },
+      {
+      name: "gitkraken",
+      title: "Gitkraken",
+      pageLink: "public/images/skills/cib-gitkraken.svg",
+      type: "git"
+      },
+      {
+      name: "github",
+      title: "Github",
+      pageLink: "../images/skills/cib-github.svg",
+      type: "git"
+      },
+      {
+      name: "bitbucket",
+      title: "Bitbucket",
+      pageLink: "../images/skills/cib-bitbucket.svg",
+      type: "git"
+      },
+      {
+      name: "gitlab",
+      title: "Gitlab",
+      pageLink: "../images/skills/cib-gitlab.svg",
+      type: "git"
       },
       {
       name: "aws",
       title: "AWS",
       pageLink: "../images/skills/cib-amazon-aws.svg",
-      type: "soft"
+      type: "dev"
+      },
+      
+      {
+      name: "css",
+      title: "CSS",
+      pageLink: "../images/skills/cib-css3.svg",
+      type: "dev"
+      },
+      {
+      name: "html",
+      title: "HTML",
+      pageLink: "public/images/skills/cib-html5.svg",
+      type: "dev"
+      },
+      {
+      name: "javascript",
+      title: "JavaScript",
+      pageLink: "../images/skills/cib-js.svg",
+      type: "dev"
+      },
+      {
+      name: "react",
+      title: "ReactJs",
+      pageLink: "../images/skills/cib-react.svg",
+      type: "dev"
+      },
+      {
+      name: "jestjs",
+      title: "JestJs",
+      pageLink: "../images/skills/cib-jest.svg",
+      type: "dev"
+      },
+      {
+      name: "mongo",
+      title: "MongoDB",
+      pageLink: "../images/skills/cib-mongodb.svg",
+      type: "dev"
+      },
+      {
+      name: "netlify",
+      title: "Netlify",
+      pageLink: "../images/skills/cib-netlify.svg",
+      type: "dev"
+      },
+      {
+      name: "node",
+      title: "NodeJs",
+      pageLink: "../images/skills/cib-node-js.svg",
+      type: "dev"
+      },
+      {
+      name: "npm",
+      title: "NPM",
+      pageLink: "../images/skills/cib-npm.svg",
+      type: "dev"
+      },
+      {
+      name: "postman",
+      title: "Postman",
+      pageLink: "../images/skills/cib-postman.svg",
+      type: "dev"
+      },
+      {
+      name: "expo",
+      title: "Expo",
+      pageLink: "../images/skills/cib-expo.svg",
+      type: "dev"
       },
       {
       name: "adobe",
       title: "Adobe XD",
-      pageLink: "https://blog.carlamontano.tech",
-      type: "soft"
+      pageLink: "../images/skills/cib-adobe-xd.svg",
+      type: "design"
+      },
+      
+      {
+      name: "jira",
+      title: "Jira",
+      pageLink: "../images/skills/cib-jira.svg",
+      type: "manage"
+      },
+      {
+      name: "confluence",
+      title: "Confluence",
+      pageLink: "public/images/skills/cib-confluence.svg",
+      type: "manage"
       },
 ];
 
