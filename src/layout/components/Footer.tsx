701609e0bbfd0d3dfdb2985877f0b4e7f70b10c3
Cmmh1101 <56605042+Cmmh1101@users.noreply.github.com>
2022-04-18 14:52:48 -0500

    footer initial work

--- src/layout/components/Footer.tsx
+++ src/layout/components/Footer.tsx
@@ -1,11 +1,35 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { Col, Container, Row } from "reactstrap";
 
-interface Props {}
-
-const Footer = (props: Props) => {
+const Footer = () => {
   return (
-    <footer>
-      <h1>initial footer set up</h1>
+    <footer className="pt-5 shadow">
+      <Container>
+        <Row className="footer">
+          <Col xs={12} md={4}>
+            <h5>Projects</h5>
+            <Link to="/" className="links">
+              Link
+            </Link>
+          </Col>
+          <Col xs={12} md={4}>
+            <h5>About</h5>
+            <Link to="/" className="links">
+              Link
+            </Link>
+          </Col>
+          <Col xs={12} md={4}>
+            <h5>Connect</h5>
+            <Link to="/" className="links">
+              Link
+            </Link>
+          </Col>
+          <Col xs={12} className="mt-5 mb-1 small">
+            All right reserved | 2022
+          </Col>
+        </Row>
+      </Container>
     </footer>
   );
 };
