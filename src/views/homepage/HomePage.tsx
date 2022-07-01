aa98dd7cc3e28e92703d1e988c0113ac3bfd6461
Cmmh1101 <56605042+Cmmh1101@users.noreply.github.com>
2022-07-01 00:13:05 -0500

    removed unused imports

--- src/views/homepage/HomePage.tsx
+++ src/views/homepage/HomePage.tsx
@@ -3,7 +3,6 @@ import ProjectsPage from "../projects/ProjectsPage";
 import About from "./components/About";
 import Contact from "./components/Contact";
 import Hero from "./components/Hero";
-import Projects from "./components/Projects";
 
 const HomePage = () => {
   return (
